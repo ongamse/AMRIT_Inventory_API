a785972120e1d0fe63f1fcc5eb7a861ddd84ad3f
Sweta Prakash <125380378+swetatech24@users.noreply.github.com>
2023-07-10 17:11:52 +0530

    API Cleanup (#28)
    * Passing Environment Variable
    
    * API Cleanup

--- src/main/java/com/iemr/inventory/controller/indent/IndentController.java
+++ src/main/java/com/iemr/inventory/controller/indent/IndentController.java
@@ -41,6 +41,7 @@ import com.iemr.inventory.service.indent.IndentService;
 import com.iemr.inventory.utils.mapper.InputMapper;
 import com.iemr.inventory.utils.response.OutputResponse;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
 @RequestMapping("/indentController")
@@ -48,24 +49,27 @@ import io.swagger.annotations.ApiParam;
 public class IndentController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-	
+
 	@Autowired
 	IndentService IndentService;
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Partial search indent items", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/partialsearchindentitems", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String createStore(@ApiParam("{\"itemName\":\"String\", \"facilityID\":\"Integer\"}") @RequestBody ItemIndent item) {
-		logger.info("partialsearchindentitems request "+item.toString());
+	public String createStore(
+			@ApiParam("{\"itemName\":\"String\", \"facilityID\":\"Integer\"}") @RequestBody ItemIndent item) {
+		logger.info("partialsearchindentitems request " + item.toString());
 		OutputResponse response = new OutputResponse();
 		try {
 
 			String saveData = "Invalid Store Type";
-			 List<ItemfacilitymappingIndent> s = IndentService.findItemIndent(item.getFacilityID(),item.getItemName().trim());
-			 saveData =s.toString();
+			List<ItemfacilitymappingIndent> s = IndentService.findItemIndent(item.getFacilityID(),
+					item.getItemName().trim());
+			saveData = s.toString();
 
 			response.setResponse(saveData);
-			logger.info("partialsearchindentitems response "+response.toString());
+			logger.info("partialsearchindentitems response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -78,18 +82,18 @@ public class IndentController {
 	}
 
 	@CrossOrigin()
+	@ApiOperation(value = "Create indent request", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/createIndentRequest", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String createIndentRequest(@RequestBody String request)
-	{
-		
+	public String createIndentRequest(@RequestBody String request) {
+
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			Indent indent= InputMapper.gson().fromJson(request, Indent.class);
-			String res= IndentService.createIndentRequest(indent);
+
+			Indent indent = InputMapper.gson().fromJson(request, Indent.class);
+			String res = IndentService.createIndentRequest(indent);
 			response.setResponse(res);
-			
+
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -99,20 +103,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Get indent history", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/getIndentHistory", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String getIndentHistory(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request)
-	{
-		logger.info("getIndentHistory request "+request);
+	public String getIndentHistory(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request) {
+		logger.info("getIndentHistory request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			Indent indent= InputMapper.gson().fromJson(request, Indent.class);
-			String res= IndentService.getIndentHistory(indent);
+
+			Indent indent = InputMapper.gson().fromJson(request, Indent.class);
+			String res = IndentService.getIndentHistory(indent);
 			response.setResponse(res);
-			logger.info("getIndentHistory response "+response.toString());
+			logger.info("getIndentHistory response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -122,20 +126,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Get order by indent id", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/getOrdersByIndentID", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String getOrdersByIndentID(@ApiParam("{\"indentID\":\"Integer\"}") @RequestBody String request)
-	{
-		logger.info("getOrdersByIndentID request "+request);
+	public String getOrdersByIndentID(@ApiParam("{\"indentID\":\"Integer\"}") @RequestBody String request) {
+		logger.info("getOrdersByIndentID request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			IndentOrder indentOrder= InputMapper.gson().fromJson(request, IndentOrder.class);
-			String res= IndentService.getOrdersByIndentID(indentOrder);
+
+			IndentOrder indentOrder = InputMapper.gson().fromJson(request, IndentOrder.class);
+			String res = IndentService.getOrdersByIndentID(indentOrder);
 			response.setResponse(res);
-			logger.info("getOrdersByIndentID response "+response.toString());
+			logger.info("getOrdersByIndentID response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -145,20 +149,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Get indent worklist", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/getIndentWorklist", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String getIndentWorklist(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request)
-	{
-		logger.info("getIndentWorklist request "+request);
+	public String getIndentWorklist(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request) {
+		logger.info("getIndentWorklist request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			IndentOrder indentOrder= InputMapper.gson().fromJson(request, IndentOrder.class);
-			String res= IndentService.getIndentWorklist(indentOrder);
+
+			IndentOrder indentOrder = InputMapper.gson().fromJson(request, IndentOrder.class);
+			String res = IndentService.getIndentWorklist(indentOrder);
 			response.setResponse(res);
-			logger.info("getIndentWorklist response "+response.toString());
+			logger.info("getIndentWorklist response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -168,20 +172,21 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Get indent order worklist", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/getIndentOrderWorklist", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String getIndentOrderWorklist(@ApiParam("{\"indentID\":\"Integer\",\"vanID\":\"Integer\" }") @RequestBody String request)
-	{
-		logger.info("getIndentOrderWorklist request "+request);
+	public String getIndentOrderWorklist(
+			@ApiParam("{\"indentID\":\"Integer\",\"vanID\":\"Integer\" }") @RequestBody String request) {
+		logger.info("getIndentOrderWorklist request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			IndentOrder indentOrder= InputMapper.gson().fromJson(request, IndentOrder.class);
-			String res= IndentService.getIndentOrderWorklist(indentOrder);
+
+			IndentOrder indentOrder = InputMapper.gson().fromJson(request, IndentOrder.class);
+			String res = IndentService.getIndentOrderWorklist(indentOrder);
 			response.setResponse(res);
-			logger.info("getIndentOrderWorklist response "+response.toString());
+			logger.info("getIndentOrderWorklist response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -191,21 +196,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
-	
+
 	@CrossOrigin()
-	@RequestMapping(value = "/issueIndent", headers = "Authorization", method = {
-			RequestMethod.POST }, produces = { "application/json" })
-	public String issueIndent(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request)
-	{
-		logger.info("issueIndent request "+request);
+	@ApiOperation(value = "Issue indent", consumes = "application/json", produces = "application/json")
+	@RequestMapping(value = "/issueIndent", headers = "Authorization", method = { RequestMethod.POST }, produces = {
+			"application/json" })
+	public String issueIndent(@ApiParam("{\"facilityID\":\"Integer\"}") @RequestBody String request) {
+		logger.info("issueIndent request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			IndentIssue[] array= InputMapper.gson().fromJson(request, IndentIssue[].class);
-			String res= IndentService.issueIndent(array);
+
+			IndentIssue[] array = InputMapper.gson().fromJson(request, IndentIssue[].class);
+			String res = IndentService.issueIndent(array);
 			response.setResponse(res);
-			logger.info("issueIndent response "+response.toString());
+			logger.info("issueIndent response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -215,20 +219,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Cancel indent order", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/cancelIndentOrder", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String cancelIndentOrder(@ApiParam("{\"indentID\":\"Integer\"}") @RequestBody String request)
-	{
-		logger.info("cancelIndentOrder request "+request);
+	public String cancelIndentOrder(@ApiParam("{\"indentID\":\"Integer\"}") @RequestBody String request) {
+		logger.info("cancelIndentOrder request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			Indent indent= InputMapper.gson().fromJson(request, Indent.class);
-			String res= IndentService.cancelIndentOrder(indent);
+
+			Indent indent = InputMapper.gson().fromJson(request, Indent.class);
+			String res = IndentService.cancelIndentOrder(indent);
 			response.setResponse(res);
-			logger.info("cancelIndentOrder response "+response.toString());
+			logger.info("cancelIndentOrder response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -238,20 +242,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
-	@RequestMapping(value = "/receiveIndent", headers = "Authorization", method = {
-			RequestMethod.POST }, produces = { "application/json" })
-	public String receiveIndent(@RequestBody String request)
-	{
-		logger.info("receiveIndent request "+request);
+	@ApiOperation(value = "Receive indent", consumes = "application/json", produces = "application/json")
+	@RequestMapping(value = "/receiveIndent", headers = "Authorization", method = { RequestMethod.POST }, produces = {
+			"application/json" })
+	public String receiveIndent(@RequestBody String request) {
+		logger.info("receiveIndent request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			Indent indent= InputMapper.gson().fromJson(request, Indent.class);
-			String res= IndentService.receiveIndent(indent);
+
+			Indent indent = InputMapper.gson().fromJson(request, Indent.class);
+			String res = IndentService.receiveIndent(indent);
 			response.setResponse(res);
-			logger.info("receiveIndent response "+response.toString());
+			logger.info("receiveIndent response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
@@ -261,20 +265,20 @@ public class IndentController {
 		 */
 		return response.toString();
 	}
-	
+
 	@CrossOrigin()
+	@ApiOperation(value = "Update indent order", consumes = "application/json", produces = "application/json")
 	@RequestMapping(value = "/updateIndentOrder", headers = "Authorization", method = {
 			RequestMethod.POST }, produces = { "application/json" })
-	public String updateIndentOrder(@RequestBody String request)
-	{
-		logger.info("updateIndentOrder request "+request);
+	public String updateIndentOrder(@RequestBody String request) {
+		logger.info("updateIndentOrder request " + request);
 		OutputResponse response = new OutputResponse();
 		try {
-			
-			Indent indent= InputMapper.gson().fromJson(request, Indent.class);
-			String res= IndentService.updateIndentOrder(indent);
+
+			Indent indent = InputMapper.gson().fromJson(request, Indent.class);
+			String res = IndentService.updateIndentOrder(indent);
 			response.setResponse(res);
-			logger.info("updateIndentOrder response "+response.toString());
+			logger.info("updateIndentOrder response " + response.toString());
 		} catch (Exception e) {
 			logger.error(e.getMessage());
 			response.setError(e);
